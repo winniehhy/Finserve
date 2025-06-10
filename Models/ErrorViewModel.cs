712d858dd5cfa9723728577e7c40449da66593bb
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2025-06-10 14:03:57 +0800

    Delete duplicated Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Finserve3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
